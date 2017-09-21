e2de6699fc268cea68dd63ae35fbea427e08df11
aaronkoe <aaron.koenigsberg98@gmail.com>
2017-09-21 12:29:13 -0500

    started movePiece function, working on helper functions

--- chess.cpp
+++ chess.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 class chessBoard {
+  private:
+    pair<int, int> findPiece(char* move);
+    void changeSpot(char* move, pair<int, int> spot);
   public:
     char board[9][9];
     chessBoard();
     void printBoard(void);
+    void movePiece(const char* move);
 };
 
 chessBoard::chessBoard() {
   int col, row;
   for (col = 0; col <= 7; col++) {
     for (row = 0; row <= 7; row++) {
-      if (row == 1 || row == 6) {
+      if (row == 1) {
         board[col][row] = 'p';
-      } else if (row == 0 || row == 7) {
+      } else if (row == 0) {
           switch (col) {
             case 0:
             case 7:
@@ -34,6 +38,29 @@ chessBoard::chessBoard() {
               break;
             case 4:
               board[col][row] = 'q';
+              break;
+          }
+      } else if (row == 6) {
+        board[col][row] = 'P';
+      } else if (row == 7) {
+          switch (col) {
+            case 0:
+            case 7:
+              board[col][row] = 'R';
+              break;
+            case 1:
+            case 6:
+              board[col][row] = 'N';
+              break;
+            case 2:
+            case 5:
+              board[col][row] = 'B';
+              break;
+            case 3:
+              board[col][row] = 'K';
+              break;
+            case 4:
+              board[col][row] = 'Q';
               break;
           }
       } else {
@@ -57,6 +84,23 @@ void chessBoard::printBoard() {
   cout << "+===================================+\n";
 }
 
+void movePiece(const char* move) {
+  pair<int, int> spot = findPiece(move);
+  changeSpot(move, spot);
+}
+
+pair<int, int> findPiece(char* move) {
+  int c = 0;
+  int col, row;
+  for (col = 0; col <= 7; col++) {
+    for (row = 0; row <= 7; row++) {
+      if (board[col][row] == move[0]) {
+        c++;
+      }
+    }
+  }
+}
+      
 
 int main() {
   chessBoard myBoard;
