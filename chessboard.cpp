6a7deaee13f2d5a1128ae7bc6c0f9acd00bf7ad8
aaronkoe <aaron.koenigsberg98@gmail.com>
2017-09-29 11:24:01 -0500

    working on move piece function and auxillary functions

--- chessboard.cpp
+++ chessboard.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
 class chessBoard {
   private:
-    pair<int, int> findPiece(char* move);
-    void changeSpot(char* move, pair<int, int> spot);
+    pair<int, int> findPiece(string move);
+    pair<int, int> findCorrectPiece(string move, pair<int, int> matches[]);
+    void movePiece (const string move);
+    void changeSpot(string move, pair<int, int> spot);
+    pair<int, int> getDestination(string move);
+    char getPiece(string move);
   public:
-    char board[9][9];
+    char board[8][8];
     chessBoard();
     void printBoard(void);
-    void movePiece(const char* move);
+    void movePiece(const string move);
 };
 
 chessBoard::chessBoard() {
@@ -84,23 +89,62 @@ void chessBoard::printBoard() {
   cout << "+===================================+\n";
 }
 
-void movePiece(const char* move) {
-  pair<int, int> spot = findPiece(move);
-  changeSpot(move, spot);
+void chessBoard::movePiece(const string move) {
+  char piece = getPiece(move);
+  pair<int, int> destination = getDestination(move);
+  pair<int, int> origination = findPiece(piece, destination);
+  changeSpot(destination, origination);
 }
 
-pair<int, int> findPiece(char* move) {
+pair<int, int> chessBoard::findPiece(char piece, pair<int, int> destination) {
   int c = 0;
-  int col, row;
+  int col, row
+  pair<int, int> matches[8];
   for (col = 0; col <= 7; col++) {
     for (row = 0; row <= 7; row++) {
-      if (board[col][row] == move[0]) {
+      if (board[col][row] == piece) {
+        matches[c] = make_pair(col, row);
         c++;
       }
     }
   }
+  if (c <= 0) {
+    throw invalid_argument("Could not find piece position");
+  } else if (c == 1) {
+    return matches[0];
+  } else {
+    return matches[findCorrectPiece(destination, matches)];
+  }
 }
-      
+
+int chessBoard::findCorrectPiece(pair<int, int> destination, pair<int, int>[] matches) {
+  int i;
+  int c = 0;
+  pair<int, int> validMoveMatches[10];
+  for (i = 0; i < 10; i++) {
+    if (destInValidMoves(destination, matches[i]) {
+      validMoveMatches[c] = matches[i];
+      c++;
+    }
+  }
+  if (c < 1) {
+    throw invalid_argument("Could not find piece position");
+  } else if (c == 1) {
+    return validMoveMatches[0];
+  } else if (c >  1) {
+    throw invalid_argument("Too many piece matches, possible invalid syntax, try adding an identifier");
+  }
+}
+
+pair<int, int> chessBoard::getDestination(string move) {
+  int col = move[1] - 'a';
+  int row = move[2] - '0';
+  return make_pair(col, row);
+}
+
+char chessBoard::getPiece(string move) {
+  return move[0];
+}
 
 int main() {
   chessBoard myBoard;
